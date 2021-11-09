be38913382a6923271a8f459fa7915755750b667
z88243310 <z88243310@gmail.com>
2021-11-09 15:58:43 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
@@ -16,9 +17,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// set express-handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 首頁
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // listening server
---
+++ views/index.hbs
@@ -0,0 +1,1 @@
+<h2>Hello world! 這是 index</h2>
