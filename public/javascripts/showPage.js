e49984d6df1bc69a99179708e00b5a3655e23157
z88243310 <z88243310@gmail.com>
2021-11-10 12:06:15 +0800

    feat: add copy pop message and adjust page style

--- public/javascripts/showPage.js
+++ public/javascripts/showPage.js
@@ -1,15 +1,19 @@
 const btnCopy = document.querySelector('#btn-copy')
 const urlLink = document.querySelector('#url-link')
+const alertInfo = document.querySelector('.alert-info')
 
 btnCopy.addEventListener('click', function onCopyButtonClicked() {
   navigator.clipboard
     .writeText(urlLink.innerText)
     .then(() => {
       console.log('Text copied to clipboard')
+      alertInfo.innerText = 'Text copied to clipboard'
+      alertInfo.classList.remove('d-none')
+      setTimeout(() => {
+        alertInfo.classList.add('d-none')
+      }, 1000)
     })
     .catch((err) => {
       console.log('Something went wrong', err)
     })
 })
-
-
