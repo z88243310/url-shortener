77ca8129af7ad91c5b19d28483e3132fdf265e76
z88243310 <z88243310@gmail.com>
2021-11-12 22:34:25 +0800

    note: commit the is-url and same url

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -19,16 +19,18 @@ router.get('/', (req, res) => {
 // Create : if exit , redirect show page
 router.post('/', (req, res) => {
   const primitiveURL = req.body.primitiveURL
-  // check url
+  // 檢查網址，不合規定直接返回首頁，顯示錯誤訊息
   if (!isUrl(primitiveURL)) return res.render('index', { invalidURL: true })
 
   Shortener.findOne({ url: primitiveURL })
     .lean()
     .then((shortener) => {
+      // 新輸入網址，先存入資料庫，再導向縮址頁面
       if (shortener === null)
         Shortener.create({ url: primitiveURL, randCode: generateRandomCode() })
           .then((shortener) => res.redirect(`/shorteners/${shortener.randCode}`))
           .catch((error) => errorHandler(error, res))
+      // 已存在此網址，直接導向縮址頁面
       else {
         res.redirect(`/shorteners/${shortener.randCode}`)
       }
